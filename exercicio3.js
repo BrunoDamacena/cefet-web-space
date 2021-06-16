ee6c63694aff1ede796ccad67b57bdb4957d2721
Bruno Damacena <damacena.bruno@gmail.com>
2021-06-16 18:18:05 -0300

    assignment 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,12 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let paragraphs = document.querySelectorAll('#aliens p');
+
+for (let paragraph of paragraphs) {
+    let button = paragraph.querySelector('button');
+    button.addEventListener('click', function() {
+        paragraph.classList.toggle('expandido');
+        button.innerHTML = button.innerHTML === '+' ? '-' : '+';
+    });
+}
